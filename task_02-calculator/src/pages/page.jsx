0910fad5a0fdb8d3efd28e0ef991d7349836478b
PRAKASH RAJA <119003888+prakashraja-0723@users.noreply.github.com>
2024-09-07 10:34:05 +0530

    task 2 completed

--- task_02-calculator/src/pages/page.jsx
+++ task_02-calculator/src/pages/page.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { FaRegClock } from "react-icons/fa6";
+import { FaGithub, FaRegClock } from "react-icons/fa6";
 import { IoCloseOutline } from "react-icons/io5";
 import CalcFrame from "../ui/CalcFrame.jsx";
 import CalcDisplay from "../ui/CalcDisplay.jsx";
@@ -30,17 +30,21 @@ const Calculator = () => {
         <div
           className={`fixed top-8 right-8 font-barlow-condensed transition-all duration-300 ease-in-out z-50`}
         >
-          <CalcHistory />
+          <CalcHistory/>
         </div>
         <CalcFrame>
-          <CalcDisplay input={input} handleKeyDown={handleKeyDown} isKeydownEnabled={isKeydownEnabled}/>
+          <CalcDisplay
+            input={input}
+            handleKeyDown={handleKeyDown}
+            isKeydownEnabled={isKeydownEnabled}
+          />
           <CalcButtons
             clear={clear}
             handleClick={handleClick}
             calculate={calculate}
           />
         </CalcFrame>
-        <ToastMsg setToastMsg={setToastMsg} toastMsg={toastMsg} />
+        <ToastMsg setToastMsg={setToastMsg} toastMsg={toastMsg}/>
       </main>
     </>
   );
--- task_02-calculator/src/ui/CalcFrame.jsx
+++ task_02-calculator/src/ui/CalcFrame.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import CalcTitle from "./CalcTitle.jsx";
+import {FaGithub} from "react-icons/fa6";
 
 const CalcFrame = ({children}) => {
 	return (
@@ -12,6 +13,19 @@ const CalcFrame = ({children}) => {
 					<div
 						className={`bg-secondary w-full h-fit rounded-xl px-8 py-8  shadow-inner shadow-secondary-content`}
 					>{children}</div>
+				</div>
+				{/*  GitHub Link */}
+				<div className="flex items-center justify-center gap-3 mt-8 text-xl">
+					<span>View code on</span>
+					<a
+						href="https://github.com/prakashraja-0723/Nezuware_Internship_Task/tree/main/task_02-calculator"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="flex items-center justify-center gap-1 text-primary-content"
+					>
+						<FaGithub/>
+						<span>@prakashraja-0723</span>
+					</a>
 				</div>
 			</div>
 			{/*<div className={`bg-gray-900`}>*/}
