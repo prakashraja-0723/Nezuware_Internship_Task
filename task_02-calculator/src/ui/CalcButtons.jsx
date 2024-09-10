9f89bf231f9ca6b3799332ec34a6877e283a241a
PRAKASH RAJA <119003888+prakashraja-0723@users.noreply.github.com>
2024-09-10 11:34:54 +0530

    task 2 completed

--- task_02-calculator/src/ui/CalcButtons.jsx
+++ task_02-calculator/src/ui/CalcButtons.jsx
@@ -1,31 +1,33 @@
 import React from 'react'
 import {MdOutlineArrowBack} from "react-icons/md";
 import {CgClose} from "react-icons/cg";
-import {FaBackspace} from "react-icons/fa";
+import {FaBackspace, FaTimes} from "react-icons/fa";
+import {FaDivide, FaEquals, FaMinus, FaPercent, FaPlus} from "react-icons/fa6";
 
 const CalcButtons = ({handleClick,clear,calculate}) => {
 	const btn_styles = `grid place-items-center bg-slate-700 text-3xl btn-primary rounded-full aspect-square w-20`;
 	return (
 		<div className={` pt-10 lg:p-0 grid grid-cols-4 gap-4`}>
-			<button className={`bg-blue-500 ${btn_styles} `} onClick={clear}>C</button>
+			<button className={`bg-blue-500 ${btn_styles} `} onClick={clear}>AC</button>
 			<button className={`bg-red-500 ${btn_styles} `} onClick={()=>handleClick('BackSpace')}><FaBackspace /></button>
-			<button className={`${btn_styles}`} onClick={()=>handleClick('/')}>/</button>
-			<button className={`${btn_styles}`} onClick={()=>handleClick('*')}><CgClose /></button>
+			<button className={`${btn_styles}`} onClick={()=>handleClick('%')}><FaPercent /></button>
+			<button className={`${btn_styles}`} onClick={()=>handleClick('/')}><FaDivide/></button>
 			<button className={`${btn_styles}`} onClick={()=>handleClick('9')}>9</button>
 			<button className={`${btn_styles}`} onClick={()=>handleClick('8')}>8</button>
 			<button className={`${btn_styles}`} onClick={()=>handleClick('7')}>7</button>
-			<button className={`${btn_styles}`} onClick={()=>handleClick('-')}>-</button>
+			<button className={`${btn_styles}`} onClick={()=>handleClick('*')}><FaTimes /></button>
 			<button className={`${btn_styles}`} onClick={()=>handleClick('6')}>6</button>
 			<button className={`${btn_styles}`} onClick={()=>handleClick('5')}>5</button>
 			<button className={`${btn_styles}`} onClick={()=>handleClick('4')}>4</button>
-			<button className={`${btn_styles}`} onClick={()=>handleClick('+')}>+</button>
+			<button className={`${btn_styles}`} onClick={()=>handleClick('-')}><FaMinus /></button>
 			<button className={`${btn_styles}`} onClick={()=>handleClick('3')}>3</button>
 			<button className={`${btn_styles}`} onClick={()=>handleClick('2')}>2</button>
 			<button className={`${btn_styles}`} onClick={()=>handleClick('1')}>1</button>
-			<button className={`${btn_styles} row-span-2 h-full rounded-3xl`} onClick={calculate}>=</button>
-			<button className={`${btn_styles}`} onClick={()=>handleClick('0')}>0</button>
+			<button className={`${btn_styles}`} onClick={()=>handleClick('+')}><FaPlus /></button>
 			<button className={`${btn_styles}`} onClick={()=>handleClick('00')}>00</button>
+			<button className={`${btn_styles}`} onClick={()=>handleClick('0')}>0</button>
 			<button className={`${btn_styles}`} onClick={()=>handleClick('.')}>.</button>
+			<button className={`bg-orange-400 text-black ${btn_styles} `} onClick={calculate}><FaEquals /></button>
 		</div>
 	)
 }
