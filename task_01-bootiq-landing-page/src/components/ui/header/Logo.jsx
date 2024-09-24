22e50827331f4d40a672c84dae310e53d4391330
PRAKASH RAJA <119003888+prakashraja-0723@users.noreply.github.com>
2024-09-24 19:57:29 +0530

    update updated logo

--- task_01-bootiq-landing-page/src/components/ui/header/Logo.jsx
+++ task_01-bootiq-landing-page/src/components/ui/header/Logo.jsx
@@ -7,7 +7,7 @@ import HighlightText3 from '@/components/ui/HighlightText3';
 const Logo = () => {
 	return (
 		<>
-			<div className="navbar-center lg:navbar-start lg:w-fit h-fit bg-white px-4 py-1 border-l-8 border-pink-400 shadow">
+			<div className="navbar-center lg:navbar-start lg:w-fit h-fit lg:bg-white px-4 py-1 lg:border-l-8 border-pink-400 lg:shadow">
 				<Link href={'/'}><HighlightText3 textStyles={'text-4xl' +
 					' pr-0'}>BootiQ</HighlightText3></Link>
 			</div>
